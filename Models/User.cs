73d36d6026272218628ae99f161d5222b0a45618
DisclaimedGoat <kyleturleywas@gmail.com>
2025-04-19 17:24:33 -0600

    Updated gitignore

--- Models/User.cs
+++ Models/User.cs
@@ -1,22 +1,39 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using FinalProject.Services;
 
 namespace FinalProject.Models;
 
 [Table("users")]
-public class User
+public class User()
 {
+	public User(string email, string username, string password) : this()
+	{
+		Email = email;
+		Username = username;
+		HashedPassword = UserService.Hash(password);
+	}
+	
 	[Column("id")]
-	public Guid Id { get; set; } = Guid.NewGuid();
+	public Guid Id { get; init; } = Guid.NewGuid();
 
+	[Column("email")]
+	public string Email { get; init; } = string.Empty;
+	
 	[Column("username")]
-	public string Username { get; set; } = string.Empty;
-
-	[Column("salt")]
-	public string Salt { get; set; } = string.Empty;
+	public string Username { get; init; } = "None";
 
 	[Column("password")]
-	public string HashedPassword { get; set; } = string.Empty;
+	public string HashedPassword { get; init; } = string.Empty;
 
 	[Column("role")]
 	public Roles Role { get; set; } = Roles.None;
+
+	[Column("created")] 
+	public DateTime Created { get; init; } = DateTime.Now;
+	
+	[Column("modified")]
+	public DateTime Modified { get; set; } = DateTime.Now;
+	
+	[Column("logged_in")]
+	public DateTime LoggedIn { get; set; } = DateTime.MinValue;
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,6 +1,88 @@
+using System.Security.Cryptography;
+using System.Security.Policy;
+using FinalProject.Models;
+
 namespace FinalProject.Services;
+
+/*
+ * Hashing algorithm and methods were gathered from here:
+ * https://stackoverflow.com/a/73125177
+ */
 
 public class UserService(AppContext appContext)
 {
+	private const int SaltSize = 16; // 128 bits
+	private const int KeySize = 32; // 256 bits
+	private const int Iterations = 50000;
+	private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA256;
+
+	private const char SegmentDelimiter = ':';
+
+	
+	public async Task InitializeAsync()
+	{
+		User admin = new("admin@admin.com", "admin", "password");
+		await SignupAsync(admin);
+	}
+	
+	public async Task<User?> AuthAsync(string username, string password)
+	{
+		User? find = appContext.Users.FirstOrDefault(user => user.Username == username);
+		if (find is null) return null;
+
+		if (!Verify(password, find.HashedPassword)) return null;
+		
+		find.LoggedIn = DateTime.Now;
+		
+		appContext.Update(find);
+		await appContext.SaveChangesAsync();
+		return find;
+
+	}
+
+	public async Task<bool> SignupAsync(User user)
+	{
+		user.LoggedIn = DateTime.Now;
+		
+		await appContext.AddAsync(user);
+		await appContext.SaveChangesAsync();
+		return true;
+	}
+
+	public static string Hash(string input)
+	{
+		byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+		byte[] hash = Rfc2898DeriveBytes.Pbkdf2(
+			input,
+			salt,
+			Iterations,
+			Algorithm,
+			KeySize
+		);
+		return string.Join(
+			SegmentDelimiter,
+			Convert.ToHexString(hash),
+			Convert.ToHexString(salt),
+			Iterations,
+			Algorithm
+		);
+	}
+
+	public static bool Verify(string input, string hashString)
+	{
+		string[] segments = hashString.Split(SegmentDelimiter);
+		byte[] hash = Convert.FromHexString(segments[0]);
+		byte[] salt = Convert.FromHexString(segments[1]);
+		int iterations = int.Parse(segments[2]);
+		HashAlgorithmName algorithm = new HashAlgorithmName(segments[3]);
+		byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(
+			input,
+			salt,
+			iterations,
+			algorithm,
+			hash.Length
+		);
+		return CryptographicOperations.FixedTimeEquals(inputHash, hash);
+	}
 	
 }
