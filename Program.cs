caa04355c98aa6c688d081ceaafd82e4bc5ba35b
DisclaimedGoat <kyleturleywas@gmail.com>
2025-04-19 23:17:18 -0600

    fixed

--- Program.cs
+++ Program.cs
@@ -30,19 +30,14 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler(
-		errorHandlingPath: "/Error",
-		createScopeForErrors: true
-	);
-
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+	//app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseStaticFiles();
 app.UseAntiforgery();
+app.UseStaticFiles();
 
 app.MapRazorComponents<App>()
 	.AddInteractiveServerRenderMode();
